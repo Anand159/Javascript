300412c432edaa5dd61276e9211f967055cdd547
Anand159 <anandkumarsoni154@gmail.com>
2025-05-30 16:41:48 +0530

    task-1 task-2

---
+++ task-1.js
@@ -0,0 +1,35 @@
+
+// Question - 1;
+
+let num;
+
+for(num=16; num <=50; num++)
+{
+    if(num%4===0){
+        console.log(num,"Quad");
+    
+    }else if(num%6===0){
+        console.log(num,"Hex");
+    }else if(num%4===0 && num%6===0){
+        console.log(num,"QuadHex")  
+    }else{
+        console.log(num);
+
+    }
+
+
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
