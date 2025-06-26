0445ee5718c8e4e07aca113ad74e6119ed5dcb4f
Anand159 <anandkumarsoni154@gmail.com>
2025-06-26 06:28:24 +0530

    delete file

--- class-work/09-06-25/task-2/app.js
+++
@@ -1,7 +0,0 @@
-document.querySelectorAll('.accordion-item h1').forEach(heading => {
-    heading.addEventListener('click', () => {
-        const paragraph = heading.nextElementSibling;
-        paragraph.classList.toggle('show');
-    });
-});
-
